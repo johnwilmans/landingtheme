6ad4ad03fc2087496892aee0d922e2487f0d58e8
johnwilmans <john.wilmans@gmail.com>
2016-08-31 11:35:56 +0100

    A very quick and dirty landing page for a client

---
+++ header.php
@@ -0,0 +1,41 @@
+<?php 
+
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Bootstrap_to_Wordpress
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    
+    
+        <!-- Google Fonts -->
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+        <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+        <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<?php wp_head(); ?>
+    
+    <!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+  </head>
+
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package landingtheme
+ */
+
+?>
+<?php get_header(); ?>
+<body>
+    <div class="container">
+    <div class="row">
+    <div class="col-sm-12">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/jwlogo.png" alt="Jo Wood Logo" class="logo">
+        </div><!-- col-sm-6 col-sm-offset-3 -->   
+    </div><!-- logo row -->
+    <div class="row">
+        <div class="col-sm-12">
+        <h1>Website coming soon</h1>
+        </div><!-- col-sm-6 col-sm-offset-3 --> 
+        </div><!-- coming soon row -->
+    <div class="row">
+        <hr />
+        <div class="col-sm-10 col-sm-offset-1">
+        <h4>Inspired by Jo Wood’s organic passion, exotic travels and rock ‘n’ roll past, the Jo Wood Collection is a sophisticated range of certified Organic products that are lovely to use, kind to the skin and great for the planet. Jo and her team have developed a luxurious range of environmentally conscious products with unusual ingredients carrying intense sensual flavours and scents from around the globe. The collection will be introduced over the next year with the first two products, an Eau de Toilette and Body oil launching mid October 2016. </h4><h4 class="preorder">Preorder opens September 30th 2016</h4>
+        </div><!-- col-sm-6 col-sm-offset-3 --> 
+        </div><!-- blurb1 row -->
+    <div class="row">
+        <hr />
+        <div class="col-sm-12">
+        <div id="mc_embed_signup">
+<form action="//jowoodorganics.us1.list-manage.com/subscribe/post?u=a53221e030402ab9aee5284fd&amp;id=929f08cdf1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<label for="mce-EMAIL">Sign up for updates and special offers</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_a53221e030402ab9aee5284fd_929f08cdf1" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+        </div><!-- col-sm-6 col-sm-offset-3 --> 
+        </div><!-- mailchimp row -->
+    <div class="row">
+        <hr />
+        <div class="col-sm-10 col-sm-offset-1">
+        <h4>“My aim has always been to create something organic that’s also exquisitely beautiful. With the Jo Wood Organics range I’ve set out to capture the spirit of the exotic places I’ve been in my life on the road, and every product has been made with love and care. We need to respect our bodies and our Earth. If we work with nature, rather than abuse it, we all reap the benefits!” 
+- Jo Wood</h4>
+        </div><!-- col-sm-6 col-sm-offset-3 --> 
+        </div><!-- blurb2 row -->
+        <div class="row">
+            <hr />
+            <div class="col-sm-10 col-sm-offset-1">
+        <div id="mc_embed_signup">
+<form action="//jowoodorganics.us1.list-manage.com/subscribe/post?u=a53221e030402ab9aee5284fd&amp;id=929f08cdf1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<label for="mce-EMAIL">Sign up for updates and special offers</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_a53221e030402ab9aee5284fd_929f08cdf1" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+        </div><!-- col-sm-6 col-sm-offset-3 --> 
+        </div><!-- mailchimp row -->
+    </div>
+    
+    
+    
+    
+
+    </body>
+</html>
